e5df11cca8ba942ed4872fc248d1de1c514de251
Peter Hellberg <peter@c7.se>
2024-10-03 00:57:49 +0200

    zig: Update build for current Zig version

--- build.zig
+++ build.zig
@@ -2,8 +2,8 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("tic", .{
-        .root_source_file = .{
-            .path = "src/tic80.zig",
-        },
+        .root_source_file = b.path(
+            "src/tic80.zig",
+        ),
     });
 }
